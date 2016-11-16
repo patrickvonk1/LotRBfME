b2f99c90159dd456801ede15016ccf44e984f4d1
patrickvonk@hotmail.com <patrickvonk@hotmail.com>
2016-11-16 09:06:00 +0100

    Real initial

---
+++ Assets/FpsCounter.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FpsCounter : MonoBehaviour {
+
+    Text mark;
+    int m_frameCounter = 0;
+    float m_timeCounter = 0.0f;
+    float m_lastFramerate = 0.0f;
+    public float m_refreshTime = 0.1f;
+
+    void Awake()
+    {
+        mark = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        if (m_timeCounter < m_refreshTime)
+        {
+            m_timeCounter += Time.deltaTime;
+            m_frameCounter++;
+        }
+        else
+        {
+            //This code will break if you set your m_refreshTime to 0 which makes no sense.
+            m_lastFramerate = (float)m_frameCounter / m_timeCounter;
+            m_frameCounter = 0;
+            m_timeCounter = 0.0f;
+        }
+
+        mark.text = "FPS: " + Mathf.Round(m_lastFramerate).ToString();
+    }
+}
---
+++ Assets/Script/UnitStats.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnitStats : MonoBehaviour {
+
+    public int CurrentHealth;
+    public int MaxHealth;
+
+    public bool CanMove;
+    public bool CanAttack;
+
+    public bool mark;
+}
